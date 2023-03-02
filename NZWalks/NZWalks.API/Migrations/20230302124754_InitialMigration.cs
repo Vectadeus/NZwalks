1eaf918ae153307fdf31f79e824ac3c3ed87b4b3
Vectadeus <mirochavchanidze4@gmail.com>
2023-03-02 17:09:30 +0400

    End of section 2

---
+++ NZWalks/NZWalks.API/Migrations/20230302124754_InitialMigration.cs
@@ -0,0 +1,29 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace NZWalks.API.Migrations
+{
+    /// <inheritdoc />
+    public partial class InitialMigration : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropColumn(
+                name: "HasLicense",
+                table: "Walks");
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AddColumn<bool>(
+                name: "HasLicense",
+                table: "Walks",
+                type: "bit",
+                nullable: false,
+                defaultValue: false);
+        }
+    }
+}
